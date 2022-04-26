458df92a08a20e22b42dc50bbaa979fed5919e68
Blake Curtsinger <ccurtsin@vols.utk.edu>
2022-04-26 08:50:59 -0400

    Initial Commit
    Started R script, created R project file, loaded various libraries

---
+++ Prediction Comp.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(shinyWidgets)
+library(plotly)
+library(fpp3)
+
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
