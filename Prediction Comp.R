b45f6e762240454fac103c1ec8236722a5714aa4
Blake Curtsinger <ccurtsin@vols.utk.edu>
2022-05-02 18:37:41 -0400

    Initial ShinyDashboard Format

--- Prediction Comp.R
+++ Prediction Comp.R
@@ -6,16 +6,35 @@ library(plotly)
 library(fpp3)
 
 CREDIT <- read.csv("credit.csv")
+CREDIT$Month <- seq(as.Date('2021/12/1'), by = "-1 month", length.out = 492)
+CREDIT$Month <- yearmonth(CREDIT$Month)
+CREDIT <- tsibble(CREDIT, index = Month)
 
 
-
-
-ui <- fluidPage(
-  
+ui <- dashboardPage(
+  dashboardHeader(title = "Galactic Credits"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem('Plotted Series and Seasonality', tabName = 'AutoSeasonDecomp', icon = icon('dashboard')),
+      menuItem('First Model', tabName = 'FirstModel', icon = icon('th'))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "AutoSeasonDecomp",
+              fluidRow(
+                h2("AutoSeasonDecomp tab content")
+              )
+      ),
+      # Second tab content
+      tabItem(tabName = "FirstModel",
+              h2("FirstModel tab content")
+      )
+    )
+  )
 )
 
-server <- function(input, output, session) {
-  
-}
+server <- function(input, output) { }
 
 shinyApp(ui, server)
